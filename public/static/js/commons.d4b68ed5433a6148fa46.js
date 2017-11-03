eae517a9b3624d819f0d0fd19d09ae6d2040e56e
liuxiaomin <liuxiaomin@sogou-inc.com>
2017-11-03 09:42:15 +0800

    fjpublish Test

---
+++ public/static/js/commons.d4b68ed5433a6148fa46.js
@@ -0,0 +1,2 @@
+webpackJsonp([27],{212:function(o,n,c){o.exports=c(88)},88:function(o,n){console.log("common.js")}},[212]);
+//# sourceMappingURL=commons.d4b68ed5433a6148fa46.js.map
--- src/controller/home.js
+++ src/controller/home.js
@@ -3,35 +3,13 @@
  */
 const version = require('../../config/version.json');
 exports.getControl = async function (ctx, next) {
+    console.log('aaaaaaa');
     try{
         await ctx.render('home/index',{
-            title:'刘晓敏个人主页',
+            title:'云门店管理系统',
             version:version
         });
     }catch (err){
         this.throw('找不到页面44', 404);
     }
-};
-exports.getArticleList = async function (ctx, next) {
-    let type = ctx.query.type,
-        list =null;
-    if(type == '诗词'){
-        list = ['123','567']
-    }else if(type == '美术') {
-        list = ['美术','美术']
-    }else if(type == '其他') {
-        list = ['其他','其他']
-    }else if(type == '技术') {
-        list = ['技术','技术']
-    }
-    try {
-        console.log(ctx.query);
-        ctx.body = {
-            list:list
-        }
-    } catch (err) {
-        ctx.body = { message: err.message };
-        ctx.status = err.status || 500;
-    }
-
 };
